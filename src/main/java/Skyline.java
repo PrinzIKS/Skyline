77fca52231abae6ce428fd4211a0ea85dca0d375
PrinzIKS <F.Prinz@iks-gmbh.com>
2022-08-25 08:43:10 +0200

    init

---
+++ src/main/java/Skyline.java
@@ -0,0 +1,12 @@
+public class Skyline {
+
+    /**
+     * @param buildings where each building contains an array of information (left x-coordinate, right x-coordinate, height)
+     * @return important coordinates to create skyline
+     */
+    public static int[][] getSkyline(int[][] buildings) {
+        // TODO write tests and implement logic
+        return new int[0][0];
+    }
+
+}
---
+++ src/test/java/SkylineTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class SkylineTest {
+
+    @Test
+    public void getSkylineWithoutBuildings() {
+        int[][] setup = {{}};
+        int[][] expected = new int[0][0];
+        int[][] actual = Skyline.getSkyline(setup);
+        assertArrayEquals(expected, actual);
+    }
+
+    // TODO implement more tests
+
+}
